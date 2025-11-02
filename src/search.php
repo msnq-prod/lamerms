3f932c926747bf9c86538238622c0a8b7f68513d
Victor <51261551+nightmareeye@users.noreply.github.com>
2025-11-02 19:21:44 +1000

    Refocus navigation and search on MVP modules

--- src/search.php
+++ src/search.php
@@ -16,20 +16,10 @@ $PAGEDATA['pageConfig'] = ["TITLE" => $PAGEDATA['term'] ? "Search result for: " 
  * Define a custom twig function to use a swtich case to generate the url the result should resolve to
  */
 $function = new TwigFunction('generate_result_url', function ($result) {
-    switch ($result['type']){
-        case "project":
-            return "/project/?id=".$result['data']['projects_id'];
-        case "location":
-            return "/location/?q=".$result['data']['locations_name'];
-        case "client":
-            return "/clients.php?q=".$result['data']['clients_name'];
-        case "page":
-            return "/cms/?p=".$result['data']['cmsPages_id'];
-        case "user":
-            return "/user.php?id=".$result['data']['users_userid'];
-        default:
-            return null;
-    }
+    return match ($result['type']) {
+        "project" => "/project/?id=" . $result['data']['projects_id'],
+        default => null,
+    };
 });
 $TWIG->addFunction($function);
 
@@ -37,20 +27,10 @@ $TWIG->addFunction($function);
  * Define a custom twig function to define the colour and text of the tag in the result
  */
 $function = new TwigFunction('generate_result_tag', function ($result) {
-    switch ($result['type']){
-        case "project":
-            return ["pink", "Project"];
-        case "location":
-            return ["indigo", "Location"];
-        case "client":
-            return ["lightblue", "Client"];
-        case "page":
-            return ["primary", "CMS Page"];
-        case "user":
-            return ["secondary", "User"];
-        default:
-            return null;
-    }
+    return match ($result['type']) {
+        "project" => ["pink", "Order"],
+        default => null,
+    };
 });
 $TWIG->addFunction($function);
 
